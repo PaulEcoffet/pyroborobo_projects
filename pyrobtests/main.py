e186c879db12a4173e31290bec8532755e318a48
Paul Ecoffet <ecoffet.paul@gmail.com>
2020-10-07 16:50:53 +0200

    Faster step by avoiding copy

--- pyrobtests/main.py
+++ pyrobtests/main.py
@@ -20,21 +20,18 @@ class WanderController(PyController):
         maxdisthalf = self.world_model.maxdistcamera // 2
         ids = self.world_model.camera_objects_ids
 
-        # %go to resource
         is_resource = (2 < ids) & (ids < 100)  # ids is in not gate nor switch and not a robot (id < 100)
         must_avoid = (ids == WALL_ID) | (ids > 1000)  # id is a wall or a robot (id > 1000)
-        if np.any(is_resource[self.world_model.fl_sensors]):
-            self.world_model.rotation = 10
-        if np.any(is_resource[self.world_model.fr_sensors]):
+        if is_resource[1]:
             self.world_model.rotation = -10
+        if is_resource[3]:
+            self.world_model.rotation = 10
 
         # even if there are resources, avoid walls if too close (half dist)
-        if np.any((dists[self.world_model.fl_sensors + self.world_model.f_sensors] < maxdisthalf)
-                  & must_avoid[self.world_model.fl_sensors + self.world_model.f_sensors]):
-            self.world_model.rotation = -10
-        if np.any((dists[self.world_model.fr_sensors] < maxdisthalf)
-                  & must_avoid[self.world_model.fr_sensors]):
+        if (dists[2] < maxdisthalf and must_avoid[2]) or (dists[1] < maxdisthalf and must_avoid[1]):
             self.world_model.rotation = 10
+        if dists[3] < maxdisthalf and must_avoid[3]:
+            self.world_model.rotation = -10
 
 
 
@@ -48,6 +45,6 @@ if __name__ == "__main__":
     rob.start()
     nbgen = 1
     for i in range(nbgen):
-        rob.update(1000)
+        rob.update(10000)
     print("over")
     Pyroborobo.close()
